fbf4d32264b38c67766296e62ed383ab1b964883
jaimecgomezz <jaimecgomezwork@gmail.com>
2020-08-15 10:01:51 -0500

    feat: 🎸 Prepare the copy-url patch

--- config.def.h
+++ config.def.h
@@ -216,6 +216,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+  // >>>>>>>>>>>>>>>>>>>> copy-url
+  // ==================== copy-url
+  // ==================== copy-url
+  // <<<<<<<<<<<<<<<<<<<< copy-url
 };
 
 /*
