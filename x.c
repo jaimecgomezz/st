f74ab0622c3d8e5408a5a2e637f01f6eaf02e838
jaimecgomezz <jaimecgomezwork@gmail.com>
2020-08-22 22:48:49 -0500

    feat: Make usage func flexible

--- x.c
+++ x.c
@@ -2110,15 +2110,38 @@ void run(void) {
 }
 
 void usage(void) {
-  die("usage: %s [-aiv] [-c class] [-f font] [-g geometry]"
-      " [-n name] [-o file]\n"
-      "          [-T title] [-t title] [-w windowid]"
-      " [[-e] command [args ...]]\n"
-      "       %s [-aiv] [-c class] [-f font] [-g geometry]"
-      " [-n name] [-o file]\n"
-      "          [-T title] [-t title] [-w windowid] -l line"
-      " [stty_args ...]\n",
-      argv0, argv0);
+  die(
+    "usage: %s"
+    "[-aiv]"
+    "[-c class]"
+    "[-f font]"
+    "[-g geometry]"
+    " [-n name]"
+    "[-o file]"
+    "\n"
+    "          [-T title]"
+    "[-t title]"
+    "[-w windowid]"
+    " [[-e] command"
+    "[args ...]]"
+    "\n"
+    "       %s"
+    "[-aiv]"
+    "[-c class]"
+    "[-f font]"
+    "[-g geometry]"
+    " [-n name]"
+    "[-o file]"
+    "\n"
+    "          [-T title]"
+    "[-t title]"
+    "[-w windowid]"
+    "-l line"
+    " [stty_args ...]"
+    "\n",
+    argv0,
+    argv0
+  );
 }
 
 int main(int argc, char *argv[]) {
